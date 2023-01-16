5bc11285537fc6c6575952a6ff7ce39496e477d2
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-01-16 18:24:07 +0300

    Commit3

--- Task1.py
+++ Task1.py
@@ -1,6 +1,22 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-text = 'вапроеалдад абв роло АБВ, абвапро'
 
-text = list(filter(lambda el: 'абв' not in el.lower(), text.split()))
-print(' '.join(text))
+import string
+string.punctuation
+
+def main():
+    text = 'вапроеалдад абв роло АБВ, абвапро апргшл'
+
+    for i in string.punctuation:
+        if i in text:
+            text = text.replace(i, f' {i}')
+    text = list(filter(lambda el: 'абв' not in el.lower(), text.split()))
+    text = ' '.join(text)
+    
+    for i in string.punctuation:
+        if i in text:
+            text = text.replace(f' {i}', i)
+    print(text)
+
+if __name__ == "__main__":
+    main()
