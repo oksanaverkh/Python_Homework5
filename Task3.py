80dd5eab50ef44de3ec7f5764964fa96931d46e2
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-01-17 02:20:14 +0300

    Commit4

--- Task3.py
+++ Task3.py
@@ -1,1 +1,91 @@
 # Создайте программу для игры в ""Крестики-нолики"".
+
+
+import os
+import random
+os.system('cls')
+
+def check_coordinates():
+    x, y = int(input()), int(input())
+    while not 0 <= x <= 2 or not 0 <= y <= 2:
+        print('Wrong number, try again')
+        x, y = int(input()), int(input())
+    return int(x), int(y)
+
+def get_empty_table():
+    print('x/y  0   1   2')
+    game_table = []
+
+    for i in range (3):
+        for j in range(4):
+            if j==0:
+                # print(f'  {i} ', end='')
+                el = f'  {i} '
+                game_table.append(el)
+            elif 0<j<3:
+                # print(' _ |', end='')
+                game_table.append(' _ |')
+            else:
+                # print(' _ ', end='')
+                game_table.append(' _ ')
+        print(*game_table)
+    print()
+    # print(game_table)
+
+def get_game_table(player_number):
+    print('x/y  0   1   2')
+    x_list, y_list = [], []
+    # game_table = []
+    print(f'Player {player_number} enters coordinates (x, y): ')
+
+    for x in range(3):
+        for y in range(4):
+            x, y = check_coordinates()
+            x_list.append(x)
+            y_list.append(y)
+            coord_list = list(zip(x_list, y_list))
+            move = (x,y,)
+            if y==0:
+                print(f'  {x} ', end='')
+            elif 0<y<3:
+                print(f' {x} |', end='')
+            else:
+                print(' _ ', end='')
+
+        for i in range(len(coord_list)):
+            while coord_list[i] == move:
+                print('Coordinates are already used, enter again')
+            print()
+
+# def victory_check()
+
+
+
+# def game(player_number):
+#     candies = 2021
+#     while candies > 0:
+#         print(f'Player {player_number} takes candies: ', end='')
+#         move = check(28)
+#         candies -= move
+#         print(f'Candies remainder = {candies}')
+#         player_number = 3-player_number
+#         while 0 < candies < 28:
+#             print(f'Player {player_number} takes candies: ', end='')
+#             move = check(candies)
+#             candies -= move
+#             player_number = 3-player_number
+#             print(f'Candies remainder = {candies}')
+#     player_number = 3-player_number
+#     print()
+#     print(f'Player {player_number} WINS!!!')
+
+def main():
+    queue = random.randint(1, 2)
+    print(f'Player {queue} starts the game')
+    get_empty_table()
+    # get_game_table(queue)
+
+    # game(queue)
+
+if __name__ == "__main__":
+    main()
--- Task4.py
+++ Task4.py
@@ -1,44 +1,31 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
-text = input('Enter text: ')
-text_list = []
-for i in text:
-    text_list.append(i)
-    
-packed_list, unpacked_list = list(), list()
-for index, element in enumerate(text_list[:-1]):
-    counter = 0
-    ind = 0
-    while text_list[ind] == text_list[ind+1]:
-        # counter = text_list.count(element)
-        counter+=1
-        ind+=1
-        packed_list.append(str(counter)+text_list[ind])
-        # unpacked_list.append(counter*element)
-packed_text = ''.join(packed_list)
-# unpacked_text = ''.join(unpacked_list)
-print(packed_text)
+def compression(string):
+    compressed_string = ''
+    i = 0
+    while i < len(string):
+        count = 1
+        while i + 1 < len(string) and string[i] == string[i + 1]:
+            count += 1
+            i += 1
+        compressed_string += str(count) + string[i]
+        i += 1
+    return compressed_string
 
-# unpacked_text = ''
+def decompression(string):
+    decompressed_string = ''
+    i = 0
+    while i < len(string)-1:
+        count = int(string[i])
+        decompressed_string += count*string[i+1]
+        i += 2
+    return decompressed_string
 
-# for i, el in enumerate(packed_list):
-#     for i in el:
-#         num = int(i)
-#         new_el = el.replace(str(num), '')
-#         unpacked_text += str(num*new_el)
-# print(unpacked_text)
-
-
-packed_list, unpacked_list = list(), list()
-counter = 0
-index = 0
-ind=0
-while text_list[index] == text_list[ind+1]:
-    # counter = text_list.count(element)
-    counter+=1
-    ind+=1
-    packed_list.append(str(counter)+text_list[index])
-    # unpacked_list.append(counter*element)
-packed_text = ''.join(packed_list)
-# unpacked_text = ''.join(unpacked_list)
-print(packed_text)
+def main():
+    text = input('Enter a text: ')
+    print()
+    print('Compressed text: ', compression(text))
+    print('Recovered text: ', decompression(compression(text)))
+ 
+if __name__ == '__main__':
+    main()
